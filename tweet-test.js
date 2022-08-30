fa3be35c56a42c74ced054dd425030486043b103
Keyanna Kiko Pausch <knpausch@gmail.com>
2022-08-30 13:50:59 -0700

    Created all properties and functions and passed all the tests

--- tweet-test.js
+++ tweet-test.js
@@ -7,67 +7,67 @@ describe('Tweet', function() {
     assert.isFunction(Tweet);
   });
 
-  it.skip('has a sender', function() {
+  it('has a sender', function() {
     var tweet = new Tweet('@LetaCodes', 'something brilliant');
 
     assert.equal(tweet.sender, '@LetaCodes');
   });
 
-  it.skip('can have a different sender', function() {
+  it('can have a different sender', function() {
     var tweet = new Tweet('@ameseee', 'something else');
 
     assert.equal(tweet.sender, '@ameseee');
   });
 
-  it.skip('has content', function() {
+  it('has content', function() {
     var tweet = new Tweet('@LetaCodes', 'something brilliant');
 
     assert.equal(tweet.content, 'something brilliant');
   });
 
-  it.skip('can have different content', function() {
+  it('can have different content', function() {
     var tweet = new Tweet('@ameseee', 'something else');
 
     assert.equal(tweet.content, 'something else');
   });
 
-  it.skip('can contain a link', function() {
+  it('can contain a link', function() {
     var tweet = new Tweet('@ameseee', 'a thought about something', 'www.somevideo.io');
 
     assert.equal(tweet.link, 'www.somevideo.io');
   })
 
-  it.skip('should have a default link value', function() {
+  it('should have a default link value', function() {
     var tweet = new Tweet('@ameseee', 'something else');
 
     assert.equal(tweet.link, 'no link provided');
   })
 
-  it.skip('starts with no likes', function() {
+  it('starts with no likes', function() {
     var tweet = new Tweet('@LetaCodes', 'something brilliant');
 
     assert.equal(tweet.likes, 0)
   });
 
-  it.skip('starts with no retweets', function() {
+  it('starts with no retweets', function() {
     var tweet = new Tweet('@maxcell', 'something wonderful');
 
     assert.equal(tweet.retweets, 0);
   });
 
-  it.skip('starts with no replies', function() {
+  it('starts with no replies', function() {
     var tweet = new Tweet('@jwanliu', 'something amazing');
 
     assert.deepEqual(tweet.replies, []);
   });
 
-  it.skip('starts with zero replies', function() {
+  it('starts with zero replies', function() {
     var tweet = new Tweet('@jwanliu', 'something amazing');
 
     assert.equal(tweet.replyCount, 0);
   });
 
-  it.skip('can be liked', function() {
+  it('can be liked', function() {
     var tweet = new Tweet('@LetaCodes', 'something brilliant');
 
     assert.equal(tweet.likes, 0);
@@ -75,7 +75,7 @@ describe('Tweet', function() {
     assert.equal(tweet.likes, 1);
   });
 
-  it.skip('can be liked many times!', function() {
+  it('can be liked many times!', function() {
     var tweet = new Tweet('@LetaCodes', 'something brilliant');
 
     assert.equal(tweet.likes, 0);
@@ -88,7 +88,7 @@ describe('Tweet', function() {
     assert.equal(tweet.likes, 5);
   });
 
-  it.skip('can be retweeted', function() {
+  it('can be retweeted', function() {
     var tweet = new Tweet('@jwanliu', 'something amazing');
 
     assert.equal(tweet.retweets, 0);
@@ -96,14 +96,14 @@ describe('Tweet', function() {
     assert.equal(tweet.retweets, 1);
   });
 
-  it.skip('can be replied to, and keep track of its replies', function() {
+  it('can be replied to, and keep track of its replies', function() {
     var tweet = new Tweet('@maxcell', 'something wonderful');
 
     tweet.reply('great tweet');
     assert.deepEqual(tweet.replies, ['great tweet']);
   });
 
-  it.skip('can be replied to many time', function() {
+  it('can be replied to many time', function() {
     var tweet = new Tweet('@maxcell', 'something wonderful');
 
     tweet.reply('great tweet');
@@ -115,7 +115,7 @@ describe('Tweet', function() {
     assert.deepEqual(tweet.replies, expected);
   });
 
-  it.skip('can count how many replies it has', function() {
+  it('can count how many replies it has', function() {
     var tweet = new Tweet('@maxcell', 'something wonderful');
 
     tweet.reply('great tweet');
--- tweet.js
+++ tweet.js
@@ -1,4 +1,33 @@
 class Tweet {
+  constructor (senderName, content, link) {
+    this.sender = senderName;
+    this.content = content;
+    if (link != undefined)
+    {
+      this.link = link;
+    }
+    else
+    {
+      this.link = "no link provided";
+    }
+    this.likes = 0;
+    this.retweets = 0;
+    this.replies = [];
+    this.replyCount = 0;
+  }
+
+  like() {
+    this.likes++;
+  }
+
+  retweet() {
+    this.retweets++;
+  }
+
+  reply(reply) {
+    this.replies.push(reply);
+    this.replyCount++;
+  }
 
 }
 
